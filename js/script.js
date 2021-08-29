9b0d7d7b0c072f53c2f7f59926bbca4cfae3317a
Riccarpa <riccardopagni99@gmail.com>
2021-08-29 16:54:19 +0200

    new message push + auto answer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,13 @@
 Vue.config.devtools = true;
-
+dayjs.extend(dayjs_plugin_customParseFormat);
 const root = new Vue({
     el: '#root',
     data: {
         data,
         visibilityIndex:0,
+        newMessage:'',
+        date: dayjs().format('DD/MM/YY HH:mm'),
+        
     },
     methods: {
         getMessageType(message){
@@ -19,6 +22,14 @@ const root = new Vue({
             
            this.visibilityIndex = index;
            
+        },
+        newMessagePush(index){
+            this.data.contacts[index].messages.push({message:this.newMessage, status:'sent',date:this.date});
+            
+            setTimeout(() => {
+                this.data.contacts[index].messages.push({message:'ok', status:'received',date:dayjs().format('DD/MM/YY HH:mm')});
+            }, 1000);
+            this.newMessage = '';
         }
       
 
